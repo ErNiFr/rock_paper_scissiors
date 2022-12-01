deedf062cc3bb2c9c2129ff17d36112a13a68e26
Niklas <erikniklasfrennfelt@gmail.com>
2022-12-01 17:48:50 +0100

    Klar, fixade spelet och skriver ut meddelanden som beskriver vad osm händer

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
+
+let cars = ["Rock", "Paper", "Scissors"];
+let x;
+let y;
+let playerScore = 0;
+let ComputerScore = 0;
+
 function getComputerChoice() {
-    let cars = ["Rock", "Paper", "Scissors"];
     x = Math.floor(Math.random() * 3);
     return cars[x];
 }
 
 const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        return `Draw! ${computerSelection} and ${computerSelection} are the same!`;
-    }
-    
-    for (let i = 0; i < 3; i++) {
-        if (playerSelection.toLowerCase() == cars[i].toLowerCase()) {
-            x = i;
+        console.log("It's a Draw")
+    } 
+    else {
+        for (let i = 0; i < 3; i++) {
+            if (playerSelection.toLowerCase() == cars[i].toLowerCase()) {
+                x = i;
+            }
+            if (computerSelection.toLowerCase() == cars[i].toLowerCase()) {
+                y = i;
+            }
         }
-        if (computerSelection.toLowerCase() == cars[i].toLowerCase()) {
-            y = i;
+        if ((x == 0 && y == 1) || (x == 1 && y == 2) || (x == 3 && y == 0)){ 
+            console.log("Computer won")
+            ComputerScore++;
+        } else {
+            console.log("You won")
+            playerScore++;
         }
     }
-    if ((x == 0 && y == 1) || (x == 1 && y == 2) || (x == 3 && y == 0)){ 
-        return `You lost! ${computerSelection} beats ${playerSelection}!`;
+    console.log(`${playerScore} - ${ComputerScore}`)
+    console.log(" ")
+}
+function game() {
+    for (let a = 0; a < 5; a++) {
+        playRound(playerSelection, getComputerChoice());
+    }
+    if (playerScore > ComputerScore) {
+        return `You won! ${playerScore} - ${ComputerScore}`;
+    } else if (playerScore < ComputerScore) {
+        return `You lost! ${playerScore} - ${ComputerScore}`;
     } else {
-        return `You won! ${playerSelection} beats ${computerSelection}!`;
+        return `Draw! ${playerScore} - ${ComputerScore}`;
     }
 }
-
-console.log(playRound(playerSelection, getComputerChoice()));
+console.log(game())
